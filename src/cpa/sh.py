413d81ee5e832ff216b48b42ffbf2fc3e7a2ff09
JaeHyeokLee98 <duckhawk1@naver.com>
2024-03-08 17:24:36 +0900

    Copy methods of sh.py from src/mqc/sh.py Copy hop_prob, hop_check, correct_dec_idc, correct_dec_edc, update_energy, write_md_output, write_sh, write_dotpop, print_init, print_step

--- src/cpa/sh.py
+++ src/cpa/sh.py
@@ -88,49 +88,129 @@ class SH(CPA):
         elif (restart == "append"):
             pass
         self.istep += 1
-        
+
         # Main MD loop
         for istep in range(self.istep, self.nsteps):
             pass
-           
-    def hop_prob(self, istep):
-        """ Routine to calculate hopping probabilities
 
-            :param integer istep: Current MD step
+    def hop_prob(self):
+        """ Routine to calculate hopping probabilities
         """
-        pass
+        # Reset surface hopping variables
+        self.rstate_old = self.rstate
+
+        self.prob = np.zeros(self.mol.nst)
+        self.acc_prob = np.zeros(self.mol.nst + 1)
+
+        self.l_hop = False
+
+        accum = 0.
+
+        for ist in range(self.mol.nst):
+            if (ist != self.rstate):
+                self.prob[ist] = - 2. * self.mol.rho.real[ist, self.rstate] * \
+                    self.mol.nacme[ist, self.rstate] * self.dt / self.mol.rho.real[self.rstate, self.rstate]
+
+                if (self.prob[ist] < 0.):
+                    self.prob[ist] = 0.
+                accum += self.prob[ist]
+            self.acc_prob[ist + 1] = accum
+        psum = self.acc_prob[self.mol.nst]
+
+        if (psum > 1.):
+            self.prob /= psum
+            self.acc_prob /= psum
 
     def hop_check(self, bo_list):
         """ Routine to check hopping occurs with random number
 
             :param integer,list bo_list: List of BO states for BO calculation
         """
-        pass
+        self.rand = random.random()
+        for ist in range(self.mol.nst):
+            if (ist == self.rstate):
+                continue
+            if (self.rand > self.acc_prob[ist] and self.rand <= self.acc_prob[ist + 1]):
+                self.l_hop = True
+                self.rstate = ist
+                bo_list[0] = self.rstate
 
     def correct_dec_idc(self):
         """ Routine to decoherence correction, instantaneous decoherence correction(IDC) scheme
         """
-        pass
+        if (self.elec_object == "coefficient"):
+            for states in self.mol.states:
+                states.coef = 0. + 0.j
+            self.mol.states[self.rstate].coef = 1. + 0.j
 
+        self.mol.rho = np.zeros((self.mol.nst, self.mol.nst), dtype=np.complex128)
+        self.mol.rho[self.rstate, self.rstate] = 1. + 0.j
 
     def correct_dec_edc(self):
         """ Routine to decoherence correction, energy-based decoherence correction(EDC) scheme
         """
-        pass
+        # Save exp(-dt/tau) instead of tau itself
+        exp_tau = np.array([1. if (ist == self.rstate) else np.exp(- self.dt / ((1. + self.edc_parameter / self.mol.ekin_qm) / \
+            np.abs(self.mol.states[ist].energy - self.mol.states[self.rstate].energy))) for ist in range(self.mol.nst)])
+        rho_update = 1.
+
+        if (self.elec_object == "coefficient"):
+            # Update coefficients
+            for ist in range(self.mol.nst):
+                # self.mol.states[self.rstate] need other updated coefficients
+                if (ist != self.rstate):
+                    self.mol.states[ist].coef *= exp_tau[ist]
+                    rho_update -= self.mol.states[ist].coef.conjugate() * self.mol.states[ist].coef
+
+            self.mol.states[self.rstate].coef *= np.sqrt(rho_update / self.mol.rho[self.rstate, self.rstate])
+
+            # Get density matrix elements from coefficients
+            for ist in range(self.mol.nst):
+                for jst in range(ist, self.mol.nst):
+                    self.mol.rho[ist, jst] = self.mol.states[ist].coef.conjugate() * self.mol.states[jst].coef
+                    self.mol.rho[jst, ist] = self.mol.rho[ist, jst].conjugate()
+
+        elif (self.elec_object == "density"):
+            # save old running state element for update running state involved elements
+            rho_old_rstate = self.mol.rho[self.rstate, self.rstate]
+            for ist in range(self.mol.nst):
+                for jst in range(ist, self.mol.nst):
+                    # Update density matrix. self.mol.rho[ist, rstate] suffers half-update because exp_tau[rstate] = 1
+                    self.mol.rho[ist, jst] *= exp_tau[ist] * exp_tau[jst]
+                    self.mol.rho[jst, ist] = self.mol.rho[ist, jst].conjugate()
+
+                if (ist != self.rstate):
+                    # Update rho[self.rstate, self.rstate] by subtracting other diagonal elements
+                    rho_update -= self.mol.rho[ist, ist]
+
+            # Update rho[self.rstate, ist] and rho[ist, self.rstate] by using rho_update and rho_old_rstate
+            # rho[self.rstate, self.rstate] automatically update by double counting
+            for ist in range(self.mol.nst):
+                self.mol.rho[ist, self.rstate] *= np.sqrt(rho_update / rho_old_rstate)
+                self.mol.rho[self.rstate, ist] *= np.sqrt(rho_update / rho_old_rstate)
 
     def update_energy(self):
         """ Routine to update the energy of molecules in surface hopping dynamics
         """
-        pass
+        # Update kinetic energy
+        self.mol.update_kinetic()
+        self.mol.epot = self.mol.states[self.rstate].energy
+        self.mol.etot = self.mol.epot + self.mol.ekin
 
     def write_md_output(self, unixmd_dir, istep):
         """ Write output files
 
             :param string unixmd_dir: PyUNIxMD directory
             :param integer istep: Current MD step
-
         """
-        pass
+        # Write the common part
+        super().write_md_output(unixmd_dir, istep)
+
+        # Write hopping-related quantities
+        self.write_sh(unixmd_dir, istep)
+
+        # Write time-derivative BO population
+        self.write_dotpop(unixmd_dir, istep)
 
     def write_sh(self, unixmd_dir, istep):
         """ Write hopping-related quantities into files
@@ -138,15 +218,24 @@ class SH(CPA):
             :param string unixmd_dir: PyUNIxMD directory
             :param integer istep: Current MD step
         """
-        pass
+        # Write SHSTATE file
+        tmp = f'{istep + 1:9d}{"":14s}{self.rstate}'
+        typewriter(tmp, unixmd_dir, "SHSTATE", "a")
+
+        # Write SHPROB file
+        tmp = f'{istep + 1:9d}' + "".join([f'{self.prob[ist]:15.8f}' for ist in range(self.mol.nst)])
+        typewriter(tmp, unixmd_dir, "SHPROB", "a")
 
     def write_dotpop(self, unixmd_dir, istep):
         """ Write time-derivative BO population
-           
+
             :param string unixmd_dir: PyUNIxMD directory
             :param integer istep: Current MD step
         """
-        pass
+        # Write NAC term in DOTPOPNAC
+        if (self.verbosity >= 1):
+            tmp = f'{istep + 1:9d}' + "".join([f'{pop:15.8f}' for pop in self.dotpopnac])
+            typewriter(tmp, unixmd_dir, "DOTPOPNAC", "a")
 
     def print_init(self, qm, mm, restart):
         """ Routine to print the initial information of dynamics
@@ -182,7 +271,32 @@ class SH(CPA):
 
             :param integer istep: Current MD step
         """
-        pass
+        ctemp = self.mol.ekin * 2. / float(self.mol.ndof) * au_to_K
+        norm = 0.
+        for ist in range(self.mol.nst):
+            norm += self.mol.rho.real[ist, ist]
+
+        # Print INFO for each step
+        INFO = f" INFO{istep + 1:>9d}{self.rstate:>5d}"
+        INFO += f"{self.mol.ekin:16.8f}{self.mol.epot:15.8f}{self.mol.etot:15.8f}"
+        INFO += f"{ctemp:13.6f}"
+        INFO += f"{norm:11.5f}"
+        print (INFO, flush=True)
+
+        # Print DEBUG1 for each step
+        if (self.verbosity >= 1):
+            DEBUG1 = f" DEBUG1{istep + 1:>7d}"
+            DEBUG1 += f"{self.rand:11.5f}"
+            for ist in range(self.mol.nst):
+                DEBUG1 += f"{self.acc_prob[ist]:12.5f} ({self.rstate}->{ist})"
+            print (DEBUG1, flush=True)
+
+        # Print event in surface hopping
+        for category, events in self.event.items():
+            if (len(events) != 0):
+                for ievent in events:
+                    print (f" {category}{istep + 1:>9d}  {ievent}", flush=True)
+        self.event["HOP"] = []
 
     def read_QM_from_file(self, istep):
         """Routine to read precomputed QM information for CPA dynamics
